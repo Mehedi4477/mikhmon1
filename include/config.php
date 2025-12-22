e5d2d0e03587632d8d6926636e955725157b2056
Mehedi4477 <93931280+Mehedi4477@users.noreply.github.com>
2025-12-23 01:52:32 +0600

    Delete include/config.php

--- include/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
-$data['mikhmon'] = array ('1'=>'mikhmon<|<M','mikhmon>|>pQ==');
-
-$data['Rasel'] = array ('1'=>'Rasel!remotemikrotik.mywire.org:44336','Rasel@|@admin','Rasel#|#qpKlnZ1ymZWfoZ8=','Rasel%Rasel WiFi Zone','Rasel^Raselwifi.net','Rasel& ','Rasel*10','Rasel(1','Rasel)','Rasel=10','Rasel@!@enable');
-
-

f289a13ac29bf2d1cad71db26dbb03affd77b028
Mehedi4477 <93931280+Mehedi4477@users.noreply.github.com>
2025-12-23 01:53:19 +0600

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,7 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+
+$data['Mehedi'] = array ('1'=>'Mehedi!remotemikrotik.mywire.org:44334','Mehedi@|@admin','Mehedi#|#amFkag==','Mehedi%Mehedi WiFi Zone','Mehedi^Mehediwifizone.com','Mehedi&Tk','Mehedi*10','Mehedi(1','Mehedi)','Mehedi=10','Mehedi@!@enable');
+$data['Rasel'] = array ('1'=>'Rasel!remotemikrotik.mywire.org:44336','Rasel@|@admin','Rasel#|#qpKlnZ1ymZWfoZ8=','Rasel%Rasel WiFi Zone','Rasel^Raselwifizone1.net','Rasel&Tk','Rasel*10','Rasel(1','Rasel)','Rasel=10','Rasel@!@enable');
